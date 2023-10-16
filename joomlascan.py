2413b8cef475f1f6d6d93c18c2fb013b6a14b597
Luca <72993234+Lunc4@users.noreply.github.com>
2023-10-17 01:34:27 +0200

    Actually ported it
    If it works, it works.
    Barely tested.
    Barely used.
    Good luck, have fun :)

--- joomlascan.py
+++ joomlascan.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import sys
-import httplib
+import http.client
 import requests
 import argparse
 from bs4 import BeautifulSoup
@@ -17,12 +17,12 @@ swversion = "0.5beta"
 
 
 def hello():
-    print "-------------------------------------------"
-    print "      	     Joomla Scan                  "
-    print "   Usage: python joomlascan.py <target>    "
-    print "    Version " + swversion + " - Database Entries " + str(len(dbarray))
-    print "         created by Andrea Draghetti       "
-    print "-------------------------------------------"
+    print("-------------------------------------------")
+    print("      	     Joomla Scan                  ")
+    print("   Usage: python joomlascan.py <target>    ")
+    print("    Version " + swversion + " - Database Entries " + str(len(dbarray)))
+    print("         created by Andrea Draghetti       ")
+    print("-------------------------------------------")
 
 
 def load_component():
@@ -39,7 +39,7 @@ def check_url(url, path="/"):
             return conn.status_code
         else:
             return 404
-    except StandardError:
+    except Exception:
         return None
 
 
@@ -48,103 +48,103 @@ def check_url_head_content_length(url, path="/"):
     try:
         conn = requests.head(fullurl, headers=useragentdesktop, timeout=timeoutconnection)
         return conn.headers["content-length"]
-    except StandardError:
+    except Exception:
         return None
 
 
 def check_readme(url, component):
     if check_url(url, "/components/" + component + "/README.txt") == 200:
-        print "\t README file found \t > " + url + "/components/" + component + "/README.txt"
+        print("\t README file found \t > " + url + "/components/" + component + "/README.txt")
 
     if check_url(url, "/components/" + component + "/readme.txt") == 200:
-        print "\t README file found \t > " + url + "/components/" + component + "/readme.txt"
+        print("\t README file found \t > " + url + "/components/" + component + "/readme.txt")
 
     if check_url(url, "/components/" + component + "/README.md") == 200:
-        print "\t README file found \t > " + url + "/components/" + component + "/README.md"
+        print("\t README file found \t > " + url + "/components/" + component + "/README.md")
 
     if check_url(url, "/components/" + component + "/readme.md") == 200:
-        print "\t README file found \t > " + url + "/components/" + component + "/readme.md"
+        print("\t README file found \t > " + url + "/components/" + component + "/readme.md")
 
     if check_url(url, "/administrator/components/" + component + "/README.txt") == 200:
-        print "\t README file found \t > " + url + "/administrator/components/" + component + "/README.txt"
+        print("\t README file found \t > " + url + "/administrator/components/" + component + "/README.txt")
 
     if check_url(url, "/administrator/components/" + component + "/readme.txt") == 200:
-        print "\t README file found \t > " + url + "/administrator/components/" + component + "/readme.txt"
+        print("\t README file found \t > " + url + "/administrator/components/" + component + "/readme.txt")
 
     if check_url(url, "/administrator/components/" + component + "/README.md") == 200:
-        print "\t README file found \t > " + url + "/administrator/components/" + component + "/README.md"
+        print("\t README file found \t > " + url + "/administrator/components/" + component + "/README.md")
 
     if check_url(url, "/administrator/components/" + component + "/readme.md") == 200:
-        print "\t README file found \t > " + url + "/administrator/components/" + component + "/readme.md"
+        print("\t README file found \t > " + url + "/administrator/components/" + component + "/readme.md")
 
 
 def check_license(url, component):
     if check_url(url, "/components/" + component + "/LICENSE.txt") == 200:
-        print "\t LICENSE file found \t > " + url + "/components/" + component + "/LICENSE.txt"
+        print("\t LICENSE file found \t > " + url + "/components/" + component + "/LICENSE.txt")
 
     if check_url(url, "/components/" + component + "/license.txt") == 200:
-        print "\t LICENSE file found \t > " + url + "/components/" + component + "/license.txt"
+        print("\t LICENSE file found \t > " + url + "/components/" + component + "/license.txt")
 
     if check_url(url, "/administrator/components/" + component + "/LICENSE.txt") == 200:
-        print "\t LICENSE file found \t > " + url + "/administrator/components/" + component + "/LICENSE.txt"
+        print("\t LICENSE file found \t > " + url + "/administrator/components/" + component + "/LICENSE.txt")
 
     if check_url(url, "/administrator/components/" + component + "/license.txt") == 200:
-        print "\t LICENSE file found \t > " + url + "/administrator/components/" + component + "/license.txt"
+        print("\t LICENSE file found \t > " + url + "/administrator/components/" + component + "/license.txt")
 
     if check_url(url, "/components/" + component + "/" + component[4:] + ".xml") == 200:
-        print "\t LICENSE file found \t > " + url + "/components/" + component + "/" + component[4:] + ".xml"
+        print("\t LICENSE file found \t > " + url + "/components/" + component + "/" + component[4:] + ".xml")
 
     if check_url(url, "/administrator/components/" + component + "/" + component[4:] + ".xml") == 200:
-        print "\t LICENSE file found \t > " + url + "/administrator/components/" + component + "/" + component[
-                                                                                                     4:] + ".xml"
+        print("\t LICENSE file found \t > " + url + "/administrator/components/" + component + "/" + component[
+                                                                                                     4:] + ".xml")
 
 
 def check_changelog(url, component):
     if check_url(url, "/components/" + component + "/CHANGELOG.txt") == 200:
-        print "\t CHANGELOG file found \t > " + url + "/components/" + component + "/CHANGELOG.txt"
+        print("\t CHANGELOG file found \t > " + url + "/components/" + component + "/CHANGELOG.txt")
 
     if check_url(url, "/components/" + component + "/changelog.txt") == 200:
-        print "\t CHANGELOG file found \t > " + url + "/components/" + component + "/changelog.txt"
+        print("\t CHANGELOG file found \t > " + url + "/components/" + component + "/changelog.txt")
 
     if check_url(url, "/administrator/components/" + component + "/CHANGELOG.txt") == 200:
-        print "\t CHANGELOG file found \t > " + url + "/administrator/components/" + component + "/CHANGELOG.txt"
+        print("\t CHANGELOG file found \t > " + url + "/administrator/components/" + component + "/CHANGELOG.txt")
 
     if check_url(url, "/administrator/components/" + component + "/changelog.txt") == 200:
-        print "\t CHANGELOG file found \t > " + url + "/administrator/components/" + component + "/changelog.txt"
+        print("\t CHANGELOG file found \t > " + url + "/administrator/components/" + component + "/changelog.txt")
 
 
 def check_mainfest(url, component):
     if check_url(url, "/components/" + component + "/MANIFEST.xml") == 200:
-        print "\t MANIFEST file found \t > " + url + "/components/" + component + "/MANIFEST.xml"
+        print("\t MANIFEST file found \t > " + url + "/components/" + component + "/MANIFEST.xml")
 
     if check_url(url, "/components/" + component + "/manifest.xml") == 200:
-        print "\t MANIFEST file found \t > " + url + "/components/" + component + "/manifest.xml"
+        print("\t MANIFEST file found \t > " + url + "/components/" + component + "/manifest.xml")
 
     if check_url(url, "/administrator/components/" + component + "/MANIFEST.xml") == 200:
-        print "\t MANIFEST file found \t > " + url + "/administrator/components/" + component + "/MANIFEST.xml"
+        print("\t MANIFEST file found \t > " + url + "/administrator/components/" + component + "/MANIFEST.xml")
 
     if check_url(url, "/administrator/components/" + component + "/manifest.xml") == 200:
-        print "\t MANIFEST file found \t > " + url + "/administrator/components/" + component + "/manifest.xml"
+        print("\t MANIFEST file found \t > " + url + "/administrator/components/" + component + "/manifest.xml")
 
 
 def check_index(url, component):
     if check_url_head_content_length(url, "/components/" + component + "/index.htm") == 200 and check_url_head(url,
                                                                                                                "/components/" + component + "/index.htm") > 1000:
-        print "\t INDEX file descriptive found \t > " + url + "/components/" + component + "/index.htm"
+        print("\t INDEX file descriptive found \t > " + url + "/components/" + component + "/index.htm")
 
     if check_url_head_content_length(url, "/components/" + component + "/index.html") == 200 and check_url_head(url,
                                                                                                                 "/components/" + component + "/index.html") > 1000:
-        print "\t INDEX file descriptive found \t > " + url + "/components/" + component + "/index.html"
+        print("\t INDEX file descriptive found \t > " + url + "/components/" + component + "/index.html")
 
     if check_url_head_content_length(url,
                                      "/administrator/components/" + component + "/INDEX.htm") == 200 and check_url_head(
         url, "/administrator/components/" + component + "/INDEX.htm") > 1000:
-        print "\t INDEX file descriptive found \t > " + url + "/administrator/components/" + component + "/INDEX.htm"
+        print("\t INDEX file descriptive found \t > " + url + "/administrator/components/" + component + "/INDEX.htm")
 
     if check_url_head_content_length(url,
                                      "/administrator/components/" + component + "/INDEX.html") == 200 and check_url_head(
         url, "/administrator/components/" + component + "/INDEX.html") > 1000:
-        print "\t INDEX file descriptive found \t > " + url + "/administrator/components/" + component + "/INDEX.html"
+        print("\t INDEX file descriptive found \t > " + url + "/administrator/components/" + component + "/INDEX.html")
 
 
 def index_of(url, path="/"):
@@ -166,7 +166,7 @@ def index_of(url, path="/"):
 
 def scanner(url, component):
     if check_url(url, "/index.php?option=" + component) == 200:
-        print "Component found: " + component + "\t > " + url + "/index.php?option=" + component
+        print("Component found: " + component + "\t > " + url + "/index.php?option=" + component)
 
         check_readme(url, component)
 
@@ -179,14 +179,14 @@ def scanner(url, component):
         check_index(url, component)
 
         if index_of(url, "/components/" + component + "/"):
-            print "\t Explorable Directory \t > " + url + "/components/" + component + "/"
+            print("\t Explorable Directory \t > " + url + "/components/" + component + "/")
 
         if index_of(url, "/administrator/components/" + component + "/"):
-            print "\t Explorable Directory \t > " + url + "/administrator/components/" + component + "/"
+            print("\t Explorable Directory \t > " + url + "/administrator/components/" + component + "/")
 
     elif check_url(url, "/components/" + component + "/") == 200:
-        print "Component found: " + component + "\t > " + url + "/index.php?option=" + component
-        print "\t But possibly it is not active or protected"
+        print("Component found: " + component + "\t > " + url + "/index.php?option=" + component)
+        print("\t But possibly it is not active or protected")
 
         check_readme(url, component)
 
@@ -199,14 +199,14 @@ def scanner(url, component):
         check_index(url, component)
 
         if index_of(url, "/components/" + component + "/"):
-            print "\t Explorable Directory \t > " + url + "/components/" + component + "/"
+            print("\t Explorable Directory \t > " + url + "/components/" + component + "/")
 
         if index_of(url, "/administrator/components/" + component + "/"):
-            print "\t Explorable Directory \t > " + url + "/administrator/components/" + component + "/"
+            print("\t Explorable Directory \t > " + url + "/administrator/components/" + component + "/")
 
     elif check_url(url, "/administrator/components/" + component + "/") == 200:
-        print "Component found: " + component + "\t > " + url + "/index.php?option=" + component
-        print "\t On the administrator components"
+        print("Component found: " + component + "\t > " + url + "/index.php?option=" + component)
+        print("\t On the administrator components")
 
         check_readme(url, component)
 
@@ -219,10 +219,10 @@ def scanner(url, component):
         check_index(url, component)
 
         if index_of(url, "/administrator/components/" + component + "/"):
-            print "\t Explorable Directory \t > " + url + "/components/" + component + "/"
+            print("\t Explorable Directory \t > " + url + "/components/" + component + "/")
 
         if index_of(url, "/administrator/components/" + component + "/"):
-            print "\t Explorable Directory \t > " + url + "/administrator/components/" + component + "/"
+            print("\t Explorable Directory \t > " + url + "/administrator/components/" + component + "/")
 
     pool.release()
 
@@ -275,18 +275,18 @@ def main(argv):
     if check_url(url) != 404:
 
         if check_url(url, "/robots.txt") == 200:
-            print "Robots file found: \t \t > " + url + "/robots.txt"
+            print("Robots file found: \t \t > " + url + "/robots.txt")
         else:
-            print "No Robots file found"
+            print("No Robots file found")
 
         if check_url(url, "/error_log") == 200:
-            print "Error log found: \t \t > " + url + "/error_log"
+            print("Error log found: \t \t > " + url + "/error_log")
         else:
-            print "No Error Log found"
+            print("No Error Log found")
 
         # Inizio la scansione dei componenti
 
-        print "\nStart scan...with %d concurrent threads!" % concurrentthreads
+        print("\nStart scan...with %d concurrent threads!" % concurrentthreads)
 
         for component in dbarray:
             # Thread Pool
@@ -298,10 +298,10 @@ def main(argv):
         while (threading.active_count() > 1):
             time.sleep(0.1)
 
-        print "End Scanner"
+        print("End Scanner")
 
     else:
-        print "Site Down, check url please..."
+        print("Site Down, check url please...")
 
 
 if __name__ == "__main__":
